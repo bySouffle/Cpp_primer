295cbbd7690c53a34666fb1cfd542dba4453e232
Souffle <47803266+bySouffle@users.noreply.github.com>
2019-12-19 22:28:24 +0800

    Update test3_3_3.cpp

--- Project1/test3_3_3.cpp
+++ Project1/test3_3_3.cpp
@@ -7,29 +7,29 @@ int main()
 {
 	vector<string> svec;
 	string s;
-	//while (cin>>s)
-	//{
-	//	//if(s!=" ")
-	//		svec.push_back(s);
-	//}
+	while (cin>>s)
+	{
+		//if(s!=" ")
+			svec.push_back(s);
+	}
 
-	//for (auto& i : svec)
-	//{
-	////	toupper(i);	//´íÎó
-	//	for (auto& j : i)
-	//	{
-	//		j=toupper(j);
-	//	}
-	//	cout << i << endl;
-	//}
+	for (auto& i : svec)
+	{
+	//	toupper(i);	//Â´Ã­ÃŽÃ³
+		for (auto& j : i)
+		{
+			j=toupper(j);
+		}
+		cout << i << endl;
+	}
 
 	// 3_20
 	vector<int> ivec = {1,2,3,4,5};
-	//int n;
-	//while (cin>>n)
-	//{
-	//	ivec.push_back(n);
-	//}
+	int n;
+	while (cin>>n)
+	{
+		ivec.push_back(n);
+	}
 	cout << ivec[0]<<ivec.size();
 
 	for (decltype(ivec.size()) index = 0; index < ivec.size(); ++index)
