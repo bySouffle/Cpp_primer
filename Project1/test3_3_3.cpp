d231d3007aac6528a1b65ef3df52f67dee67afb5
Souffle <47803266+bySouffle@users.noreply.github.com>
2019-12-20 08:37:42 +0800

    Update test3_3_3.cpp

--- Project1/test3_3_3.cpp
+++ Project1/test3_3_3.cpp
@@ -23,8 +23,9 @@ int main()
 		cout << i << endl;
 	}
 
+	
 	// 3_20
-	vector<int> ivec = {1,2,3,4,5};
+	vector<int> ivec;
 	int n;
 	while (cin>>n)
 	{
@@ -32,7 +33,25 @@ int main()
 	}
 	cout << ivec[0]<<ivec.size();
 
-	for (decltype(ivec.size()) index = 0; index < ivec.size(); ++index)
+	cout << "相邻：";
+	if (ivec.size() % 2 == 0)
+	{
+		for (decltype(ivec.size()) index_1=0; index_1 < ivec.size(); index_1 += 2)
+		{
+			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
+		}
+	}
+
+	else {
+		for (decltype(ivec.size()) index_1 = 0; index_1 < ivec.size()-1; index_1 += 2)
+		{
+			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
+		}
+
+	}
+	
+	cout <<endl<< "第一项和最后一项："<<endl;
+	for (decltype(ivec.size()) index = 0; index < ivec.size()/2; ++index)
 	{
 		cout << ivec[index] + ivec[ivec.size() - index-1] << endl;
 	}
