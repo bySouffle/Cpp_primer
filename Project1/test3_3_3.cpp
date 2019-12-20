f2c7a0442e45a9d6f46a15356760f1a22b9e1eb3
夜 兮 <1284290038@qq.com>
2019-12-20 13:42:45 +0800

    合并_2

--- Project1/test3_3_3.cpp
+++ Project1/test3_3_3.cpp
@@ -7,51 +7,32 @@ int main()
 {
 	vector<string> svec;
 	string s;
-	while (cin>>s)
-	{
-		//if(s!=" ")
-			svec.push_back(s);
-	}
+	//while (cin>>s)
+	//{
+	//	//if(s!=" ")
+	//		svec.push_back(s);
+	//}
 
-	for (auto& i : svec)
-	{
-	//	toupper(i);	//´íÎó
-		for (auto& j : i)
-		{
-			j=toupper(j);
-		}
-		cout << i << endl;
-	}
+	//for (auto& i : svec)
+	//{
+	////	toupper(i);	//错误
+	//	for (auto& j : i)
+	//	{
+	//		j=toupper(j);
+	//	}
+	//	cout << i << endl;
+	//}
 
-	
 	// 3_20
-	vector<int> ivec;
-	int n;
-	while (cin>>n)
-	{
-		ivec.push_back(n);
-	}
+	vector<int> ivec = {1,2,3,4,5};
+	//int n;
+	//while (cin>>n)
+	//{
+	//	ivec.push_back(n);
+	//}
 	cout << ivec[0]<<ivec.size();
 
-	cout << "相邻：";
-	if (ivec.size() % 2 == 0)
-	{
-		for (decltype(ivec.size()) index_1=0; index_1 < ivec.size(); index_1 += 2)
-		{
-			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
-		}
-	}
-
-	else {
-		for (decltype(ivec.size()) index_1 = 0; index_1 < ivec.size()-1; index_1 += 2)
-		{
-			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
-		}
-
-	}
-	
-	cout <<endl<< "第一项和最后一项："<<endl;
-	for (decltype(ivec.size()) index = 0; index < ivec.size()/2; ++index)
+	for (decltype(ivec.size()) index = 0; index < ivec.size(); ++index)
 	{
 		cout << ivec[index] + ivec[ivec.size() - index-1] << endl;
 	}
