d3abb209c95ca0fd096e1674b0be98c499f09088
夜 兮 <1284290038@qq.com>
2019-12-20 15:06:50 +0800

    test3.4.1

--- Project1/test3_3_3.cpp
+++ Project1/test3_3_3.cpp
@@ -1,41 +1,117 @@
-#include<iostream>
-#include<vector>
-
-using namespace std;
-
-int main()
-{
-	vector<string> svec;
-	string s;
-	//while (cin>>s)
-	//{
-	//	//if(s!=" ")
-	//		svec.push_back(s);
-	//}
-
-	//for (auto& i : svec)
-	//{
-	////	toupper(i);	//閿欒
-	//	for (auto& j : i)
-	//	{
-	//		j=toupper(j);
-	//	}
-	//	cout << i << endl;
-	//}
-
-	// 3_20
-	vector<int> ivec = {1,2,3,4,5};
-	//int n;
-	//while (cin>>n)
-	//{
-	//	ivec.push_back(n);
-	//}
-	cout << ivec[0]<<ivec.size();
-
-	for (decltype(ivec.size()) index = 0; index < ivec.size(); ++index)
-	{
-		cout << ivec[index] + ivec[ivec.size() - index-1] << endl;
-	}
-
-
-}
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//
+//<<<<<<< HEAD
+////int main()
+////{
+////	vector<string> svec;
+////	string s;
+////	//while (cin>>s)
+////	//{
+////	//	//if(s!=" ")
+////	//		svec.push_back(s);
+////	//}
+////
+////	//for (auto& i : svec)
+////	//{
+////	////	toupper(i);	//错误
+////	//	for (auto& j : i)
+////	//	{
+////	//		j=toupper(j);
+////	//	}
+////	//	cout << i << endl;
+////	//}
+////
+////	// 3_20
+////	vector<int> ivec;
+////	int n;
+////	while (cin>>n)
+////	{
+////		ivec.push_back(n);
+////	}
+////	cout << ivec[0]<<ivec.size();
+////
+////	cout << "相邻：";
+////	if (ivec.size() % 2 == 0)
+////	{
+////		for (decltype(ivec.size()) index_1=0; index_1 < ivec.size(); index_1 += 2)
+////		{
+////			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
+////		}
+////	}
+////
+////	else {
+////		for (decltype(ivec.size()) index_1 = 0; index_1 < ivec.size()-1; index_1 += 2)
+////		{
+////			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
+////		}
+////
+////	}
+////	
+////	cout <<endl<< "第一项和最后一项："<<endl;
+////	for (decltype(ivec.size()) index = 0; index < ivec.size()/2; ++index)
+////	{
+////		cout << ivec[index] + ivec[ivec.size() - index-1] << endl;
+////	}
+////
+////
+////}
+//=======
+//int main()
+//{
+//	vector<string> svec;
+//	string s;
+//	while (cin>>s)
+//	{
+//		//if(s!=" ")
+//			svec.push_back(s);
+//	}
+//
+//	for (auto& i : svec)
+//	{
+//	//	toupper(i);	//麓铆脦贸
+//		for (auto& j : i)
+//		{
+//			j=toupper(j);
+//		}
+//		cout << i << endl;
+//	}
+//
+//	
+//	// 3_20
+//	vector<int> ivec;
+//	int n;
+//	while (cin>>n)
+//	{
+//		ivec.push_back(n);
+//	}
+//	cout << ivec[0]<<ivec.size();
+//
+//	cout << "鐩搁偦锛?;
+//	if (ivec.size() % 2 == 0)
+//	{
+//		for (decltype(ivec.size()) index_1=0; index_1 < ivec.size(); index_1 += 2)
+//		{
+//			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
+//		}
+//	}
+//
+//	else {
+//		for (decltype(ivec.size()) index_1 = 0; index_1 < ivec.size()-1; index_1 += 2)
+//		{
+//			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
+//		}
+//
+//	}
+//	
+//	cout <<endl<< "绗竴椤瑰拰鏈�鍚庝竴椤癸細"<<endl;
+//	for (decltype(ivec.size()) index = 0; index < ivec.size()/2; ++index)
+//	{
+//		cout << ivec[index] + ivec[ivec.size() - index-1] << endl;
+//	}
+//
+//
+//}
+//>>>>>>> d231d3007aac6528a1b65ef3df52f67dee67afb5
