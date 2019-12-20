a12497e4578ca60ec5ba945cfb3c3e20224a45e7
夜 兮 <1284290038@qq.com>
2019-12-20 08:34:04 +0800

    test3_3_3

--- Project1/test3_3_3.cpp
+++ Project1/test3_3_3.cpp
@@ -24,15 +24,33 @@ int main()
 	//}
 
 	// 3_20
-	vector<int> ivec = {1,2,3,4,5};
-	//int n;
-	//while (cin>>n)
-	//{
-	//	ivec.push_back(n);
-	//}
+	vector<int> ivec;
+	int n;
+	while (cin>>n)
+	{
+		ivec.push_back(n);
+	}
 	cout << ivec[0]<<ivec.size();
 
-	for (decltype(ivec.size()) index = 0; index < ivec.size(); ++index)
+	cout << "相邻：";
+	if (ivec.size() % 2 == 0)
+	{
+		for (decltype(ivec.size()) index_1=0; index_1 < ivec.size(); index_1 += 2)
+		{
+			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
+		}
+	}
+
+	else {
+		for (decltype(ivec.size()) index_1 = 0; index_1 < ivec.size()-1; index_1 += 2)
+		{
+			cout << ivec[index_1] + ivec[index_1 + 1]<<" ";
+		}
+
+	}
+	
+	cout <<endl<< "第一项和最后一项："<<endl;
+	for (decltype(ivec.size()) index = 0; index < ivec.size()/2; ++index)
 	{
 		cout << ivec[index] + ivec[ivec.size() - index-1] << endl;
 	}
